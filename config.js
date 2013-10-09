f37fa6aaecee545a9db3ee81c0ea84bdc193c966
Scott Smith <scottksmith95@gmail.com>
2013-10-08 20:27:29 -0700

    config changes

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	fromNumber: '2096457152'
-  , accountSid: 'ACa2242f57b20c086056e48488494740f2'
-  , authToken: '7969b8b06ae78ee68f1219e6e1bdd530'
+	fromNumber: ''
+  , accountSid: ''
+  , authToken: ''
 }
